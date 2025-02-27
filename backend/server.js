711a895ac642c3e0aa0c758c860ec92133aa9aed
Marvin Murmann <marv.murmann@gmx.net>
2025-02-27 23:37:44 +0100

    added rudimentary backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/characters', async (req, res) => {
+    const [rows] = await pool.execute('SELECT * FROM character');
+    res.json(rows);
+  });
+
+const pool = new Pool({
+    user: 'module_manager',
+    host: 'localhost',
+    database: 'modules',
+    password: 'neverwinternights',
+    port: 5432,
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
