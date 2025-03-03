3fea84bf1dcd726d494ce308f026ded7d3fd7e09
Marvin Murmann <marv.murmann@gmx.net>
2025-03-03 10:52:50 +0100

    added create panel

--- Module Manager/src/components/CampaignList.vue
+++ Module Manager/src/components/CampaignList.vue
@@ -9,6 +9,21 @@
         </li>
       </ul>
     </div>
+    <div v-if="showCreatePanel" class="create-panel">
+      <h2>Create New Campaign</h2>
+      <form @submit.prevent="createCampaign">
+        <label>
+          Name:
+          <input v-model="newCampaign.name" required />
+        </label>
+        <label>
+          Description:
+          <textarea v-model="newCampaign.description"></textarea>
+        </label>
+        <button type="submit">Create</button>
+        <button @click="showCreatePanel = false" type="button">Cancel</button>
+      </form>
+    </div>
 </template>
   
 <script setup>
@@ -20,6 +35,12 @@ const campaignStore = useCampaignStore();
 onBeforeMount(() => {
     campaignStore.fetchCampaigns();
 });
+
+const createCampaign = async () => {
+  await campaignStore.createCampaign(newCampaign.value);
+  showCreatePanel.value = false; // Close panel after creation
+  newCampaign.value = { name: '', description: '' }; // Reset form
+};
 </script>
   
 <style scoped>
